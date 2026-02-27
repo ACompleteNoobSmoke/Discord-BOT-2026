e6aad76f968ed9edb58a9bd932084e1ae8002342
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2026-02-27 16:29:00 -0600

    Removed model package

--- src/main/java/model/SPOTIFY_RESPONSE_TYPE.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public enum SPOTIFY_RESPONSE_TYPE {
-
-    TRACK,
-    TRACKS,
-    ALBUMS,
-    ARTIST
-}
--- src/main/java/model/SpotifyResponses.java
+++
@@ -1,8 +0,0 @@
-package model;
-
-import java.util.List;
-
-public class SpotifyResponses {
-
-    List<SpotifyResponse> spotifyResponseList;
-}
